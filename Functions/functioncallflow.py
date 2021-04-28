829edca92d15cbf051391b26925fcb36d4f34bc4
Rambhupal Chowdary Payyavula <34411783+bhupal143@users.noreply.github.com>
2021-04-27 21:33:57 -0700

    Created function call flow
    This program describes the call function flow.

---
+++ Functions/functioncallflow.py
@@ -0,0 +1,19 @@
+def a():
+    print('a() starts')
+    b()
+    print('a() returns')
+def b():
+    print('b() starts')
+    c()
+    print('b() returns')
+def c():
+    print('c() starts')
+    d()
+    print('c() returns')
+def d():
+    print('d() starts')
+    print('d() retunrs')
+
+a()
+
+
